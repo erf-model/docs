787431180220a354329601c94fae298ad1b5a4e1
asalmgren <asalmgren@lbl.gov>
2023-07-27 20:36:58 +0000

    Deployed from erf-model/ERF

--- structWENO__MZQ3.js
+++ structWENO__MZQ3.js
@@ -9,6 +9,7 @@ var structWENO__MZQ3 =
     [ "eps", "structWENO__MZQ3.html#ad02f88cd05b6e31261fb3380887a2099", null ],
     [ "g1", "structWENO__MZQ3.html#ad6c988889194e31f809b878a8b75390e", null ],
     [ "g2", "structWENO__MZQ3.html#a074870251e62994daec13919faff1576", null ],
+    [ "g3", "structWENO__MZQ3.html#a97cfb78afa5c496fb3194141f284556c", null ],
     [ "m_phi", "structWENO__MZQ3.html#a106585bb6e78f7027658c9c56f3ae892", null ],
     [ "tol", "structWENO__MZQ3.html#af1d30022fe1ebe59529f45843dff3ae5", null ]
 ];
