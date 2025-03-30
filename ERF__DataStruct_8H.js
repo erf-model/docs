789759f76f416b95f7c2fc75dcb1a5868fcac694
asalmgren <asalmgren@lbl.gov>
2025-03-30 14:54:52 +0000

    Deployed from erf-model/ERF

--- ERF__DataStruct_8H.js
+++ ERF__DataStruct_8H.js
@@ -20,7 +20,7 @@ var ERF__DataStruct_8H =
     ] ],
     [ "AMREX_ENUM", "ERF__DataStruct_8H.html#a01afbed431da562282cc06eef6fb9c88", null ],
     [ "AMREX_ENUM", "ERF__DataStruct_8H.html#a31ae75ca03a26e6ad638ba3bdf2246eb", null ],
-    [ "AMREX_ENUM", "ERF__DataStruct_8H.html#a1e6f00289ee5047b087ffde4cf5c49dc", null ],
+    [ "AMREX_ENUM", "ERF__DataStruct_8H.html#a7596cfba18ef8aeb937cfab4f9018550", null ],
     [ "AMREX_ENUM", "ERF__DataStruct_8H.html#acdc002a92ddacce9c7f56a850ef3ea8c", null ],
     [ "AMREX_ENUM", "ERF__DataStruct_8H.html#a167e3ccc662c308c584d8722a013a25c", null ],
     [ "AMREX_ENUM", "ERF__DataStruct_8H.html#a527d8c0024ac44ed7d9d58920ba18924", null ],
