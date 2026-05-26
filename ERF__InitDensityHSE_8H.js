38b0c8a3d7293bc9c7d6dfcfc71efce356be34ec
asalmgren <asalmgren@lbl.gov>
2026-05-26 21:39:24 +0000

    Deployed from erf-model/ERF

--- ERF__InitDensityHSE_8H.js
+++ ERF__InitDensityHSE_8H.js
@@ -3,6 +3,6 @@ var ERF__InitDensityHSE_8H =
     [ "erf_init_const_dens_and_linear_th_hse", "ERF__InitDensityHSE_8H.html#a34ab896915f05a0d580a4b1eb1258d0a", null ],
     [ "erf_init_const_dens_and_th_hse", "ERF__InitDensityHSE_8H.html#ab69c03b611b51f3fe4531d97e268b994", null ],
     [ "erf_init_const_dens_hse", "ERF__InitDensityHSE_8H.html#a06963d7ee774ab4b864478b38cee7ead", null ],
-    [ "erf_init_dens_hse", "ERF__InitDensityHSE_8H.html#a2e1d042f9d82e4b78d00e99ab25e9311", null ],
-    [ "erf_init_dens_hse_moist", "ERF__InitDensityHSE_8H.html#aa8787512ee3cc1e62a429eeae8ff5eeb", null ]
+    [ "erf_init_dens_hse", "ERF__InitDensityHSE_8H.html#a563ff3438ac7dc75b8645cd317b27dd3", null ],
+    [ "erf_init_dens_hse_moist", "ERF__InitDensityHSE_8H.html#a4abbdf605678b66401e2bb6b8b547d2c", null ]
 ];
